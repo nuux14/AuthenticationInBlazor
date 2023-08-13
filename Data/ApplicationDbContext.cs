ecae6fe79f0f3b48d5a31d878231f581f1625a7b
Mubarek <nuux14@gmail.com>
2023-08-13 07:02:37 -0500

    Initial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using AuthenticationInBlazor.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationInBlazor.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+ protected override void OnModelCreating(ModelBuilder builder){
+        base.OnModelCreating(builder);
+        builder.Entity<IdentityRole>().HasData
+        (
+            new IdentityRole{
+                Id ="1",
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole{
+                Id ="2",
+                Name = "Users",
+                NormalizedName = "USERS"
+            },
+                        new IdentityRole{
+                Id ="3",
+                Name = "Clerks",
+                NormalizedName = "CLERKS"
+            }
+        );
+    }
+}
---
+++ Data/Migrations/20230812112530_AddeAppUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenticationInBlazor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddeAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
